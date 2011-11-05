debc3c396b8f23810f461c549257c08641924360
smittex@gmail.com <smittex@gmail.com@964e0b4e-ddae-a778-a86e-30ad330303b1>
2011-11-05 17:08:37 +0000

    test

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <?php
 
-
+echo 'test';
 
 ?>
 <html>
-  <body>
+<body>
 <form action="sample.php" method="post">
-    <table>
-      <tr>
-        <td>Maker Name:</td>
-        <td><input type="text" name="maker_name" /></td>
-      </tr>
-      <tr>
-        <td>Project Name:</td>
-        <td><input type="text" name="project_name" /></td>
-      </tr>
-      <tr>
-        <td>Completion Date:</td>
-        <td><input type="text" name="completion" /></td>
-      </tr>
-      <tr>
-        <td>Materials Used:</td>
-        <td><input type="text" name="materials" /></td>
-      </tr>
-      <tr>
-        <td>Process & Function</td>
-        <td><input type="text" name="process" /></td>
-      </tr>
-      <tr>
-        <td colspan="2"><input type="submit" value="Submit" id="submit" /></td>
-      </tr>
-    <table>
+<table>
+	<tr>
+		<td>Maker Name:</td>
+		<td><input type="text" name="maker_name" /></td>
+	</tr>
+	<tr>
+		<td>Project Name:</td>
+		<td><input type="text" name="project_name" /></td>
+	</tr>
+	<tr>
+		<td>Completion Date:</td>
+		<td><input type="text" name="completion" /></td>
+	</tr>
+	<tr>
+		<td>Materials Used:</td>
+		<td><input type="text" name="materials" /></td>
+	</tr>
+	<tr>
+		<td>Process & Function</td>
+		<td><input type="text" name="process" /></td>
+	</tr>
+	<tr>
+		<td colspan="2"><input type="submit" value="Submit" id="submit" /></td>
+	</tr>
+</table>
 </form>
-  </body>
+
+</body>
 </html>
