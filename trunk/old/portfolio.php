9e90d3910a761bc852bcecf1859b21205d519e37
smittex@gmail.com <smittex@gmail.com@964e0b4e-ddae-a778-a86e-30ad330303b1>
2012-02-13 00:40:16 +0000

    Moved unnecessary files to a subfolder

---
+++ trunk/old/portfolio.php
@@ -0,0 +1,15 @@
+<?php
+
+$font = './includes/fonts/Verdana.ttf';
+
+// First we create our bounding box for the first text
+$bbox = imagettfbbox(10, 0, $font, $_GET['text']);
+
+// This is our cordinates for X and Y
+$x = $bbox[4] - $bbox[0];
+$y = $bbox[1] - $bbox[5];
+
+echo 'x: ' .$x;
+echo '<br />';
+echo 'y: ' .$y;
+?>
